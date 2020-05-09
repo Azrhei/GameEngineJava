a89df1415be563b9f613e5ad9c65035079ccd09b
Quinn J Roundy <qjroundy@gmail.com>
2020-05-08 22:42:32 -0500

    Creating initial Project and Main starting class Setting up dependencies: added LWJGL 3.2.3 and all subpackages. First Build

--- GameEngineJava/src/main/java/com/azrhei/gameenginejava/Main/Main.java
+++ GameEngineJava/src/main/java/com/azrhei/gameenginejava/Main/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.azrhei.gameenginejava.Main;
 
 /**
@@ -12,6 +7,7 @@ package com.azrhei.gameenginejava.Main;
 public class Main {
     public static void main(String[] args)
     {
+     
     }
     
 }
